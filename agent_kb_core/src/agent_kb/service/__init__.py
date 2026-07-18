8b285de1999404913cbd049e0a29b6f04cc74374
kanlyn1985 <1322479668@qq.com>
2026-07-18 13:29:51 +0800

    Phase 7: export hardened service adapter

--- agent_kb_core/src/agent_kb/service/__init__.py
+++ agent_kb_core/src/agent_kb/service/__init__.py
@@ -1,5 +1,13 @@
 """Application and HTTP service adapters."""
 
 from .api import AgentKBService, ServiceHealth, create_http_server
+from .secure_api import HardenedAgentKBService, HardenedServiceConfig, create_secure_http_server
 
-__all__ = ["AgentKBService", "ServiceHealth", "create_http_server"]
+__all__ = [
+    "AgentKBService",
+    "HardenedAgentKBService",
+    "HardenedServiceConfig",
+    "ServiceHealth",
+    "create_http_server",
+    "create_secure_http_server",
+]
