b14aa620291b0ccfdda9a7246060f863937b81f7
kanlyn1985 <1322479668@qq.com>
2026-07-18 13:29:19 +0800

    Phase 7: export background job queue

--- agent_kb_core/src/agent_kb/runtime/__init__.py
+++ agent_kb_core/src/agent_kb/runtime/__init__.py
@@ -1,5 +1,11 @@
 """Runtime controls used by hardened service adapters."""
 
+from .jobs import BackgroundJob, SQLiteJobQueue
 from .rate_limit import RateLimitDecision, TokenBucketRateLimiter
 
-__all__ = ["RateLimitDecision", "TokenBucketRateLimiter"]
+__all__ = [
+    "BackgroundJob",
+    "RateLimitDecision",
+    "SQLiteJobQueue",
+    "TokenBucketRateLimiter",
+]
